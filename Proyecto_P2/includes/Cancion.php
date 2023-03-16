52ec3fde9a547867e38b768811f58f15ed1070b9
Marcsaga1 <marc.segales1710@gmail.com>
2023-03-16 19:15:39 +0100

    Clase Cancion,php

--- Proyecto_P2/includes/Cancion.php
+++ Proyecto_P2/includes/Cancion.php
@@ -1,5 +1,6 @@
 <?php
 
+//use LDAP\Result;
 
 class Cancion{
 
@@ -12,9 +13,9 @@ class Cancion{
     private $rutaImagen;
 
 
-    private function __construct($id, $nombre, $genero, $nombreAlbum, $duracion, $rutaCancion, $rutaImagen){
+    private function __construct($nombre, $genero, $nombreAlbum, $duracion, $rutaCancion, $rutaImagen){
         
-        $this->id = $id;
+        $this->id = null;
         $this->nombre = $nombre;
         $this->genero = $genero;
         $this->nombreAlbum = $nombreAlbum;
@@ -22,6 +23,133 @@ class Cancion{
         $this->rutaCancion = $rutaCancion;
         $this->rutaImagen = $rutaImagen;
     }
+
+
+    public static function buscarPorAlbum($nombreAlbum){
+
+        $lista = [];
+        $conn = Aplicacion::getInstance()->getConexionBd();
+        $query = sprintf("SELECT * FROM canciones c WHERE c.nombreAlbum='%s'", $nombreAlbum);
+        $result = $conn->query($query);
+
+        if(!$result->num_rows > 0){
+            error_log("Error BD ({$conn->errno}): {$conn->error}");
+        }else{
+            
+            foreach($result as $rs){
+                $cancion = new Cancion($rs['id'], $rs['nombre'], 
+                $rs['genero'], $rs['nombreAlbum'], $rs['duracion'], 
+                $rs['rutaCancion'], $rs['rutaImagen']);
+                $lista[] = $cancion;
+            }
+
+            $result->free();
+        }
+
+        return $lista;
+    }
+
+
+    public static function buscarPorGenero($genero){
+
+        $lista = [];
+        $conn = Aplicacion::getInstance()->getConexionBd();
+        $query = sprintf("SELECT * FROM canciones c WHERE c.genero='%s'", $genero);
+        $result = $conn->query($query);
+
+        if(!$result->num_rows > 0){
+            error_log("Error BD ({$conn->errno}): {$conn->error}");
+        }else{
+            
+            foreach($result as $rs){
+                $cancion = new Cancion($rs['id'], $rs['nombre'], 
+                $rs['genero'], $rs['nombreAlbum'], $rs['duracion'], 
+                $rs['rutaCancion'], $rs['rutaImagen']);
+                $lista[] = $cancion;
+            }
+
+            $result->free();
+        }
+
+        return $lista;
+    }
+    
+    
+    public static function buscarPorNombre($nombre){
+
+        $conn = Aplicacion::getInstance()->getConexionBd();
+        $query = sprintf("SELECT * FROM canciones c WHERE c.nombre='%s'", $nombre);
+        $result = $conn->query($query);
+
+        if(!$result){
+            error_log("Error BD ({$conn->errno}): {$conn->error}");
+        }else{
+
+            $rs = $result->fetch_assoc();
+            if($rs){
+                $cancion = new Cancion($rs['id'], $rs['nombre'], 
+                $rs['genero'], $rs['nombreAlbum'], $rs['duracion'], 
+                $rs['rutaCancion'], $rs['rutaImagen']);
+            }
+            $rs->free();
+        }
+
+        return $cancion;
+    } 
+
+
+    private static function inserta($cancion){
+
+        $result = false;
+        $conn = Aplicacion::getInstance()->getConexionBd();
+        $query = sprintf("INSERT INTO canciones(nombreCancion, genero, nombreAlbum, duracion, rutaCancion, rutaImagen) VALUES ('%s', '%s', '%s', '%s','%s', '%s')"
+        ,$conn->real_escape_string($cancion->nombre)
+        , $conn->real_escape_string($cancion->genero)
+        , $conn->real_escape_string($cancion->nombreAlbum)
+        , $conn->real_escape_string($cancion->duracion)
+        , $conn->real_escape_string($cancion->rutaCancion)
+        , $conn->real_escape_string($cancion->rutaImagen)
+        );
+
+        if($conn->query($query)){
+            $cancion->id = $conn->insert_id;
+            $result = true;
+        }else{
+            error_log("Error BD ({$conn->errno}): {$conn->error}");
+        }
+        return $result;
+    }
+
+
+    private static function actualiza($cancion)
+    {
+        $result = false;
+        $conn = Aplicacion::getInstance()->getConexionBd();
+        $query=sprintf("UPDATE canciones U SET nombreCancion = '%s', genero='%s', nombreAlbum='%s', duracion='%s', rutaCancion='%s', rutaImagen='%s' WHERE U.idCancion=%d"
+            , $conn->real_escape_string($cancion->nombre)
+            , $conn->real_escape_string($cancion->genero)
+            , $conn->real_escape_string($cancion->nombreAlbum)
+            , $conn->real_escape_string($cancion->duracion)
+            , $conn->real_escape_string($cancion->rutaCancion)
+            , $conn->real_escape_string($cancion->rutaImagen)
+            , $cancion->id
+        );
+        if ( $conn->query($query) ) {
+            $result = true;
+        } else {
+            error_log("Error BD ({$conn->errno}): {$conn->error}");
+        }
+        return $result;
+    }
+
+
+    public function guarda(){
+        if($this->id !== null){
+            return self::actualiza($this);
+        }
+        return self::inserta($this);
+    }
+
                   
     public function getId(){
         return $this->id;
