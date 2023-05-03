95771830956a36d3dabaf25dcf4b57f6b95e0b09
Alonso <alonsr01@ucm.es>
2023-05-03 11:02:21 +0200

    incluir css swipe

--- Proyecto_P3/includes/vistas/plantillas/plantilla.php
+++ Proyecto_P3/includes/vistas/plantillas/plantilla.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?=RUTA_CSS?>/buscador.css">
+    <link rel="stylesheet" href="<?=RUTA_CSS?>/swipe.css">
+
 
 </head>
 <body>
