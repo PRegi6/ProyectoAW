f542affa0fe0d370c70ecfd0d67b535e8f7e0e62
rodrii02 <101178007+rodrii02@users.noreply.github.com>
2023-05-04 11:32:01 +0200

    Delete Proyecto_P3 directory

--- Proyecto_P3/gestionAnuncios.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once __DIR__."/includes/config.php";
-    
-    $tituloPagina = "Anuncios";
-    $contenidoPrincipal = es\ucm\fdi\aw\Admin::mostrarAnuncios($_SESSION['email']);
-
-    require RAIZ_APP."/vistas/plantillas/plantilla.php";
--- Proyecto_P3/includes/vistas/comun/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-        <h1>Contacto</h1>
-        <img src="<?= RUTA_IMGS ?>/logoTwitter.png" alt="twitter" class="logoTw">
-        <img src="<?= RUTA_IMGS ?>/logoInsta.png" alt="instagram" class="logoInsta">
-</footer>
